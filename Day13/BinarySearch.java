7461022df02931870b8c5537b23830faabcd4fcb
keerthi996 <84732206+keerthi996@users.noreply.github.com>
2023-03-14 09:25:47 +0530

    changes

--- Day13/BinarySearch.java
+++ Day13/BinarySearch.java
@@ -3,18 +3,17 @@ import java.util.Arrays;
 import java.util.*;
 class BinarySearch{
     static int binary(int[] a,int l,int n,int x){
-        if(n>=1){
+        if(n>=l){
             int mid=l+(n-1)/2;
             if(a[mid]==x){
                 return mid;
             }
-            else if(a[mid]>x){
-                return binary(a,0,mid,x);
+            if(a[mid]>x){
+                return binary(a,1,mid-1,x);
             }
             else{
                 return binary(a,mid+1,n,x);
             }
-
         }
         return -1;
         
@@ -24,18 +23,21 @@ class BinarySearch{
         System.out.println("Enter number of elements");
         int n=sc.nextInt();
         int[] a=new int[n];
+        System.out.println("Enter elements:");
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
         }
         Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
         System.out.println("Enter the element u want to search");
         int x=sc.nextInt();
-        int res=binary(a,0,n,x);
-        if(res>0){
-            System.out.println("Element is found at:"+binary(a,0,n,x));
+        int res=binary(a,0,n-1,x);
+        if(res==-1){
+            System.out.println("Element not found");
         }
         else{
-            System.out.println("Element not found");
+            
+            System.out.println("Element is found at:"+res);
         }
     }
 }
