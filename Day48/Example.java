7ca559dad8006a37177ab972d9c4ad1d745ae8cf
keerthi996 <84732206+keerthi996@users.noreply.github.com>
2023-04-17 18:32:00 +0530

    commit

---
+++ Day48/Example.java
@@ -0,0 +1,13 @@
+//Scanner class for different datatypes
+import java.util.*;
+class Example{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        Boolean b=sc.nextBoolean();
+        Long l=sc.nextLong();
+        Float f=sc.nextFloat();
+        String s=sc.nextLine();
+
+    }
+}
