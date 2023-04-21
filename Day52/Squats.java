3435fdad70427e37711c6ae9a7a8708f206b0898
keerthi996 <84732206+keerthi996@users.noreply.github.com>
2023-04-21 23:14:12 +0530

    commit

---
+++ Day52/Squats.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+
+class Squats
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int x=sc.nextInt();
+		    System.out.println(x*15);
+		}
+	}
+}
