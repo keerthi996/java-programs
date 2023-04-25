7cbda797adbc7471c97a8ee37d079aac84cb5e49
keerthi996 <84732206+keerthi996@users.noreply.github.com>
2023-04-25 21:25:23 +0530

    commit

---
+++ Day56/UpcastingAndDowncasting.java
@@ -0,0 +1,32 @@
+
+
+class Parent {
+    String name;
+    void method ()
+    {
+        System.out.println("Method from Parent");
+    }
+
+} 
+class Child extends Parent {
+    int id;
+    @Override void method()
+    {
+    System.out.println("Method from Child");
+    }
+}
+public class UpcastingAndDowncasting {
+    public static void main(String [] args)
+    {
+        Parent p = new Child();
+        p.name = "GeeksforGeeks" ;
+        System.out.println(p.name);
+        p.method();
+        Child c = (Child)p;
+        c.id = 1;
+        System.out.println(c.name);
+        System.out.println(c.id);
+        c.method();
+
+    }
+}
